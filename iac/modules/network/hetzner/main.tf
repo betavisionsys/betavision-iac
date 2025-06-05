1fa49aa82392b6f30f342c57769b6511d2f865e1
João Paulo de Souza <joao@betavision.com.br>
2025-06-05 15:31:43 -0300

    Remover recursos de sub-rede do módulo Hetzner, simplificando a configuração da rede. Atualizar outputs para refletir a remoção do `subnet_id` e `network_cidr`.

--- iac/modules/network/hetzner/main.tf
+++ iac/modules/network/hetzner/main.tf
@@ -11,10 +11,3 @@ resource "hcloud_network" "main" {
   name     = var.name
   ip_range = var.cidr
 }
-
-resource "hcloud_subnet" "main" {
-  network_id    = hcloud_network.main.id
-  type          = "cloud"
-  network_zone  = var.location
-  ip_range      = var.cidr
-}

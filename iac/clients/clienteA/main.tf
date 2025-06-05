78b42dd603eff18ae2f4420870da27a15abd435b
João Paulo de Souza <joao@betavision.com.br>
2025-06-04 22:55:40 -0300

    Add main.tf with sample Terraform configuration for clienteA

--- iac/clients/clienteA/main.tf
+++ iac/clients/clienteA/main.tf
@@ -1,27 +1,18 @@
-terraform {
-  required_providers {
-    hcloud = {
-      source = "hetznercloud/hcloud"
-    }
-  }
-}
-
-provider "hcloud" {
+provider "hetzner" {
   token = var.hetzner_token
 }
 
-module "network" {
-  source = "../../modules/network/hetzner"
+resource "hcloud_network" "clienteA_network" {
   name     = var.network_name
-  cidr     = var.network_cidr
-  location = var.location
+  ip_range = var.network_cidr
 }
 
-module "compute" {
-  source = "../../modules/compute/hetzner"
-  network_id          = module.network.network_id
-  image               = var.server_image
-  server_type         = var.server_type
-  instance_count     = var.server_count
-  ssh_key_fingerprint = var.ssh_key_fingerprint
+resource "hcloud_server" "clienteA_server" {
+  count           = var.server_count
+  name            = "clienteA-server-${count.index}"
+  image           = var.server_image
+  server_type     = var.server_type
+  location        = var.location
+  ssh_keys        = [var.ssh_key_fingerprint]
+  network_ids     = [hcloud_network.clienteA_network.id]
 }
